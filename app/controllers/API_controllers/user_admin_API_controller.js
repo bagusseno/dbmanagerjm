9e2ebb8956fe928af0d59c5210b3bad224914096
Bagus Seno Pamungkas <bagusseno@Baguss-MacBook-Pro.local>
2020-03-11 21:19:59 +0700

    updated organization structure of controllers

---
+++ app/controllers/API_controllers/user_admin_API_controller.js
@@ -0,0 +1,40 @@
+'use strict'
+
+var template_engine = require('../libs/template_engine_lib.js')
+var User_model = require('../models/user_model.js')
+
+exports.manage_all_event_head = (req, res) => {
+
+}
+
+exports.manage_all_event = (req, res) => {
+    
+}
+
+exports.manage_event = (req, res) => {
+    
+}
+
+exports.manage_all_audience_by_event_head_id = (req, res) => {
+    
+}
+
+exports.manage_all_audience_by_audience_head_id = (req, res) => {
+    
+}
+
+exports.manage_audience = (req, res) => {
+    
+}
+
+exports.manage_all_audience_head = (req, res) => {
+    
+}
+
+exports.manage_all_audience = (req, res) => {
+    
+}
+
+exports.manage_all_meta_index = (req, res) => {
+    
+}
---
+++ app/controllers/API_controllers/user_public_API_controller.js
@@ -0,0 +1,13 @@
+'use strict'
+
+var template_engine = require('../libs/template_engine_lib.js')
+var user_model = require('../models/user_model.js')
+var presence_model = require('../models/presence_model.js')
+
+exports.auth = (req, res) => {
+
+}
+
+exports.presence = (req, res) => {
+    
+}
